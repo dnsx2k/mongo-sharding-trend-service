317d6bc30341e4b49d932da50ce01577d7d4fe82
Mateusz Pasterny <92987016+dnsx2k@users.noreply.github.com>
2022-11-04 12:25:30 +0100

    Rebalancing methods implementation
    MoveIn, MoveOut

--- pkg/rebalancing/service.go
+++ pkg/rebalancing/service.go
@@ -3,6 +3,7 @@ package rebalancing
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -24,9 +25,57 @@ func New(mongoClientPrimary *mongo.Client, mongoClientHot *mongo.Client) Rebalan
 }
 
 func (sc *serviceCtx) MoveIn(ctx context.Context, IDs []string) error {
+	collection := sc.mongoClientPrimary.Database("customSharding").Collection("products")
+	cursor, err := collection.Find(ctx, bson.M{"id": bson.M{"$in": IDs}})
+	if err != nil {
+		return err
+	}
+
+	var products []interface{}
+	if err := cursor.All(ctx, &products); err != nil {
+		return err
+	}
+
+	collectionHot := sc.mongoClientHot.Database("customSharding").Collection("products")
+	_, err = collectionHot.InsertMany(ctx, products)
+	if err != nil {
+		return err
+	}
+
+	//TODO: call lookup service
+
+	_, err = collection.DeleteMany(ctx, bson.M{"id": bson.M{"$in": IDs}})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (sc *serviceCtx) MoveOut(ctx context.Context, IDs []string) error {
+	collection := sc.mongoClientHot.Database("customSharding").Collection("products")
+	cursor, err := collection.Find(ctx, bson.M{"id": bson.M{"$in": IDs}})
+	if err != nil {
+		return err
+	}
+
+	var products []interface{}
+	if err := cursor.All(ctx, &products); err != nil {
+		return err
+	}
+
+	collectionHot := sc.mongoClientPrimary.Database("customSharding").Collection("products")
+	_, err = collectionHot.InsertMany(ctx, products)
+	if err != nil {
+		return err
+	}
+
+	//TODO: call lookup service
+
+	_, err = collectionHot.DeleteMany(ctx, bson.M{"id": bson.M{"$in": IDs}})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
